63c930fee66b03aa51a307efa4e04f8911d06034
edduardohuamani24 <eduardoHuamaniC2019@gmail.com>
2020-12-20 00:00:26 -0500

    Estilos del header y el nav completos, falta el body e implementar js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let marvel_comics = document.getElementById("marvel-comics");
+let dc_comics = document.getElementById("dc-comics");
+
+marvel_comics.addEventListener("click", function () {
+  window.location = "marvel.html";
+});
+
+dc_comics.addEventListener("click", function () {
+  window.location = "dc.html";
+});
