dd124283a0f60f6570d740e5bc595359c9a1c5cd
edduardohuamani24 <eduardoHuamaniC2019@gmail.com>
2021-05-24 12:49:58 -0500

    Se cambió el nombre de las clases de las redes sociales y de igual manera al archivo Js que valida si el input debe aparecer o cerrarse en vista para dispositivos.

--- js/validateInputSearch.js
+++ js/validateInputSearch.js
@@ -4,11 +4,11 @@ import {
   input_search,
   showSearchInput,
 } from "./showInputSearch.js";
-const validateIconSearch = () => {
+const validateInputSearch = () => {
   if (input_search.classList.contains("search-responsive")) {
     showSearchInput();
   } else {
     closeSearchInput();
   }
 };
-icon_search.addEventListener("click", validateIconSearch);
+icon_search.addEventListener("click", validateInputSearch);
