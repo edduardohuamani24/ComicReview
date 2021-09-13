043bbbe6b2e8843f9ce5e25e751c4978c1b086d7
edduardohuamani24 <eduardoHuamaniC2019@gmail.com>
2021-09-13 06:09:48 -0500

    Prooyecto de prueba con webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+//Instalar paquete de node js para abotener ubicación
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./js/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|gif|svg|webp)$/i,
+        use: ["file-loader?name=img/[name].[ext]", "image-webpack-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: "index.html",
+      //Usar como plantilla el index.html para crear otro con los scrips
+      template: "./index.html",
+    }),
+  ],
+};
