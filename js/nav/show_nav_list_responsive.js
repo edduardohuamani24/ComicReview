97f65dce01c6d6755ed703af1dd936e6522e2738
edduardohuamani24 <eduardoHuamaniC2019@gmail.com>
2021-10-26 22:55:01 -0500

    Cambios en nobmres de propiedades y se aumento z-index para correguir el error del botón con forma de lupa

--- js/nav/show_nav_list_responsive.js
+++ js/nav/show_nav_list_responsive.js
@@ -3,14 +3,16 @@ import { hideScrollbar } from "./hide_scrollbar.js";
 
 const ham = document.querySelector(".nav__hamburger");
 const list = document.querySelector(".nav__list");
-const search_button = document.getElementById("search_button");
+const search_button = document.getElementById("search-button");
+const search_input = document.getElementById("search-input");
 export const openCloseHam = () => {
   //El botón hamburguesa se le agregá un evento list que es una función anónimma.
   ham.addEventListener("click", () => {
     //Usamos el método toggle mediante la propiedad a la lista
     //El método toggle permite agregar la clase nav__list-activate si existe y si ya la tiene la remueve.
     list.classList.toggle("nav__list-activate");
-    search_button.classList.toggle("show-button");
+    search_button.classList.toggle("elem-hidden");
+    search_input.classList.toggle("elem-hidden");
     rotateRods();
     /*Quitar barra espaciadora horizontal */
     hideScrollbar();
